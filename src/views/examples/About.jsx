fc216818b05b08676e90123f5620724c5159125f
Djavani <djavanirgomes@gmail.com>
2021-07-09 22:13:49 -0300

    primeiro commit

---
+++ src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = props => (
+    <h1 className='About'>
+        Componente About
+        <p>O nosso sistema foi criado</p>
+    </h1>
+)
+
+export default About
